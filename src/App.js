bf47d2f9cd9ced28964d81ba56625eef4b1ee078
Ramkrishna1998 <bhattramkrishna98@gmail.com>
2022-01-29 20:21:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='p-14'>
+      <div class="max-w-2xl mx-auto py-4 px-4 sm:py-4 sm:px-6 lg:px-8">
+        <p class="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl uppercase sm:tracking-tight lg:text-6xl">Tailwind CSS</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
